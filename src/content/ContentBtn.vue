cadd50f152c144f96cf4294a00a4afab708442a4
Yan Dobrovolskiy <bloodwoodwood@gmail.com>
2023-04-30 20:00:48 +0300

    init

---
+++ src/content/ContentBtn.vue
@@ -0,0 +1,11 @@
+<script setup lang='ts'>
+
+</script>
+
+<template>
+<div class='helper-button'>Gogog</div>
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/content/index.ts
@@ -0,0 +1,11 @@
+import { createApp } from 'vue'
+import { v4 as uuidv4 } from 'uuid';
+import './index.css'
+import App from './ContentBtn.vue'
+
+const app = document.createElement('div')
+const id = uuidv4()
+app.id = id
+document.body.append(app)
+
+createApp(App).mount(`[id="${id}"]`)
